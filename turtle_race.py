375b42364c3fd1f7d0535281e07beaec5d95baa3
woojinhong03 <hongwoojin471@gmail.com>
2022-04-10 20:18:07 +0900

    Create turtle_race.py

---
+++ turtle_race.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 20:03:34 2022
+
+@author: wooji
+"""
+
+import turtle
+t1 = turtle.Turtle()
+t1.shape("turtle")
+
+t2 = turtle.Turtle()
+t2.shape("turtle")
+
+t1.color("blue")
+t1.goto(0,200)
+t2.color("red")
+t2.goto(0,200)
