4659db8c0e4e72dfefffa3374ba25ea8f06d18be
woojinhong03 <72974628+woojinhong03@users.noreply.github.com>
2022-04-14 13:35:26 +0900

    Create 220414.py

---
+++ 220414.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 13:29:05 2022
+
+@author: wooji
+"""
+
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+def square(length):
+    for i in range(4):
+        t.forward(length)
+        t.left(90)
+
+square(100)
+t.up()
+t.goto(200,0)
+t.down()
+square(100)
+    
