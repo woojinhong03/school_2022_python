3d74e44551a1e6fca51b41a2e2f36abb624ec012
woojinhong03 <72974628+woojinhong03@users.noreply.github.com>
2022-04-17 12:47:49 +0900

    Update turtle_race.py

--- turtle_race.py
+++ turtle_race.py
@@ -5,14 +5,18 @@ Created on Sun Apr 10 20:03:34 2022
 import random
 import turtle
 
+#예측 성공 여부 출력용 터틀
 t = turtle.Turtle()
 
+#첫 번째 터틀 선언
 t1 = turtle.Turtle()
 t1.shape("turtle")
 
+# 두 번째 터틀 선언
 t2 = turtle.Turtle()
 t2.shape("turtle")
 
+#각 위치로 터틀 옮기기
 t1.color("blue")
 t1.up()
 t1.goto(0,200)
@@ -22,9 +26,11 @@ t2.up()
 t2.goto(0,-200)
 t2.down()
 
+#터틀 거리 변수 선언
 t1r = 0
 t2r = 0
 
+#사용자에게 알맞는 예측 결과를 입력받음
 while(True):
     text = turtle.textinput("경주게임", "파랑 or 빨강")
     if(text == "파랑" or text == "빨강"):
@@ -32,6 +38,7 @@ while(True):
     else:
         continue
 
+#터틀 경주 시작
 while(t1r < 300 and t2r < 300):
     
     t1m = random.randint(1,10)
@@ -58,5 +65,4 @@ else:
     t.write("예측 실패")
 
 turtle.exitonclick()
-
 turtle.bye()
