e26e343a0da14578a17d1cee7099e693c4337c21
woojinhong03 <72974628+woojinhong03@users.noreply.github.com>
2022-04-28 15:16:23 +0900

    Update 220428과제2.py

--- 220428과제2.py
+++ 220428과제2.py
@@ -4,4 +4,21 @@ Created on Thu Apr 28 13:54:04 2022
 
 @author: wooji
 """
-import 
+import turtle
+
+aList = []
+
+for i in range(4):
+    a1 = turtle.Turtle()
+    a1.shape("turtle")
+    aList.append(a1)
+
+for i in range(4):
+    aList[i].left(90*i)
+    
+    for j in range(4):
+        aList[i].fd((i*40)+40)
+        aList[i].left(90)
+        
+turtle.exitonclick()
+turtle.bye()
